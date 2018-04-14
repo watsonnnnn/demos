58bfaa1229db3139026f90c3152d527adc7a7a6e
lujing <lujing@cn95598.com>
2018-04-14 17:01:45 +0800

    add promise

---
+++ promise/index.js
@@ -0,0 +1,22 @@
+function PPromise(fn){
+    this.successCallbacks = [];
+    this.value = null;
+    this.status = 'PENDING';
+    var self = this;
+
+    function resolve(v){
+        setTimeout(function(){
+            self.status = 'FULFILLED';
+            self.successCallbacks.forEach(f => {
+                self.value = f(v);
+            });
+        }, 0);
+    }
+
+    function then(f){
+        this.successCallbacks.push(f);
+        return this;
+    }
+
+    fn(resolve);
+}
